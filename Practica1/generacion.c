2928a7e6909c9231d1c2dbf291cc43070b770905
martagm1 <martagm_97@hotmail.com>
2019-10-14 18:30:35 +0200

    cositas principio

--- Practica1/generacion.c
+++ Practica1/generacion.c
@@ -2,71 +2,96 @@
 #include "generacion.h"
 
 
-/*
-Código para el principio de la sección .bss.
-Con seguridad sabes que deberás reservar una variable entera para guardar el
-puntero de pila extendido (esp). Se te sugiere el nombre __esp para esta variable.
-*/
+
 void escribir_cabecera_bss(FILE* fpasm){
 
 }
 
 
-/*
-Declaración (con directiva db) de las variables que contienen el texto de los
-mensajes para la identificación de errores en tiempo de ejecución.
-En este punto, al menos, debes ser capaz de detectar la división por 0.
-*/
 void escribir_subseccion_data(FILE* fpasm){
 
 }
 
 
-/*
-Para ser invocada en la sección .bss cada vez que se quiera declarar una
-variable:
-- El argumento nombre es el de la variable.
-- tipo puede ser ENTERO o BOOLEANO (observa la declaración de las constantes
-del principio del fichero).
-- Esta misma función se invocará cuando en el compilador se declaren
-vectores, por eso se adjunta un argumento final (tamano) que para esta
-primera práctica siempre recibirá el valor 1.
-*/
 void declarar_variable(FILE* fpasm, char * nombre, int tipo, int tamano){
 
 }
 
 
-/*
-Para escribir el comienzo del segmento .text, básicamente se indica que se
-exporta la etiqueta main y que se usarán las funciones declaradas en la librería
-olib.o
-*/
 void escribir_segmento_codigo(FILE* fpasm){
 
 }
 
 
-/*
-En este punto se debe escribir, al menos, la etiqueta main y la sentencia que
-guarda el puntero de pila en su variable (se recomienda usar __esp).
-*/
 void escribir_inicio_main(FILE* fpasm){
 
 }
 
 
-/*
-Al final del programa se escribe:
-- El código NASM para salir de manera controlada cuando se detecta un error
-en tiempo de ejecución (cada error saltará a una etiqueta situada en esta
-zona en la que se imprimirá el correspondiente mensaje y se saltará a la
-zona de finalización del programa).
-- En el final del programa se debe:
-·Restaurar el valor del puntero de pila (a partir de su variable __esp)
-·Salir del programa (ret).
-*/
 void escribir_fin(FILE* fpasm){
 
 }
 
+
+void escribir_operando(FILE* fpasm, char* nombre, int es_variable){
+
+}
+
+void asignar(FILE* fpasm, char* nombre, int es_variable){
+
+}
+
+/* OPERACIONES */
+void sumar(FILE* fpasm, int es_variable_1, int es_variable_2){
+
+}
+void restar(FILE* fpasm, int es_variable_1, int es_variable_2){
+
+}
+void multiplicar(FILE* fpasm, int es_variable_1, int es_variable_2){
+
+}
+void dividir(FILE* fpasm, int es_variable_1, int es_variable_2){
+
+}
+void o(FILE* fpasm, int es_variable_1, int es_variable_2){
+
+}
+void y(FILE* fpasm, int es_variable_1, int es_variable_2){
+
+}
+void cambiar_signo(FILE* fpasm, int es_variable){
+
+}
+
+void no(FILE* fpasm, int es_variable, int cuantos_no){
+
+}
+
+/* FUNCIONES COMPARATIVAS */
+void igual(FILE* fpasm, int es_variable1, int es_variable2, int etiqueta){
+
+}
+void distinto(FILE* fpasm, int es_variable1, int es_variable2, int etiqueta){
+
+}
+void menor_igual(FILE* fpasm, int es_variable1, int es_variable2, int etiqueta){
+
+}
+void mayor_igual(FILE* fpasm, int es_variable1, int es_variable2, int etiqueta){
+
+}
+void menor(FILE* fpasm, int es_variable1, int es_variable2, int etiqueta){
+
+}
+void mayor(FILE* fpasm, int es_variable1, int es_variable2, int etiqueta){
+
+}
+
+/* FUNCIONES DE ESCRITURA Y LECTURA */
+void leer(FILE* fpasm, char* nombre, int tipo){
+
+}
+void escribir(FILE* fpasm, int es_variable, int tipo){
+
+}
