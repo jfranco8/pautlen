e42a6759a1c1c350112c0f1508a31a57ea8a5787
jfranco8 <jesus.franco@estudiante.uam.es>
2020-06-07 19:02:36 +0200

    aritmeticas2 va dpm

--- FinalJesus/pruebas_finales.sh
+++ FinalJesus/pruebas_finales.sh
@@ -2,9 +2,14 @@
 make clean
 make
 
-./pruebaCompilador prueba_final/aritmeticas1.alf prueba_final/aritmeticas1_mi_salida.asm
+# ./pruebaCompilador prueba_final/aritmeticas1.alf prueba_final/aritmeticas1_mi_salida.asm
+#
+# nasm -g -o prueba_final/aritmeticas1_mi_salida.o -f elf32 prueba_final/aritmeticas1_mi_salida.asm
+# gcc -o prueba_final/aritmetica1 -m32 prueba_final/aritmeticas1_mi_salida.o prueba_final/alfalib.o
 
-nasm -g -o prueba_final/aritmeticas1_mi_salida.o -f elf32 prueba_final/aritmeticas1_mi_salida.asm
-gcc -o prueba_final/aritmetica1 -m32 prueba_final/aritmeticas1_mi_salida.o prueba_final/alfalib.o
+./pruebaCompilador prueba_final/aritmeticas2.alf prueba_final/aritmeticas2_mi_salida.asm
+
+nasm -g -o prueba_final/aritmeticas2_mi_salida.o -f elf32 prueba_final/aritmeticas2_mi_salida.asm
+gcc -o prueba_final/aritmetica2 -m32 prueba_final/aritmeticas2_mi_salida.o prueba_final/alfalib.o
 
 # make clean
